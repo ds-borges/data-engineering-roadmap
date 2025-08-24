85ca741615ec050be5ee32d436cc86d8c49a12ad
ds-borges <diego97sousa@gmail.com>
2025-08-23 23:44:41 -0300

    acresccentando comentarios

--- Bootcamp-Intensivo-Python/Aula01/desafio.py
+++ Bootcamp-Intensivo-Python/Aula01/desafio.py
@@ -1,12 +1,17 @@
 #Desafio
 
+#Inserção do nome
 nome = input("Digite seu nome: ")
-#print("\n")
-salario = float(input("Digite seu salario: "))
-#print("\n")
-bonus = float(input("Digite seu bonus em %: "))
-#print("\n")
 
+#Inserção do valor do salario e tipagem para float
+salario = float(input("Digite seu salario: "))
+
+#Inserção do bonus para calculo do salariofinal e tipagem para float
+bonus = float(input("Digite seu bonus em %: "))
+
+#Cálculo do salário final transformando o valor do bonus em porcentagem para aaplicação do bonus
 valor_total = salario + (salario * (bonus/100)) 
+
+#Impressão do nome e salário
 #print(nome +", seu salário com bonus é: " + str(valor_total)  + " !") 
 print(f"O usuário {nome} possui bonus de: {bonus}% e vai reeceber salario de: {valor_total} !") 
