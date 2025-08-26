6f97353ca632ac6bc8f22c286d6692a00e30892e
ds-borges <diego97sousa@gmail.com>
2025-08-26 09:17:05 -0300

    exercicio 1 final

--- Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_01.py
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_01.py
@@ -1,9 +1,12 @@
-# Exercício 01                                                                             #
+############################### Exercício 01 ################################
+
 # Escreva um programa que soma dois números inteiros inseridos pelo usuário. #
 
+# Pegando os 2 valores
+print(" Digite 2 valores para serem somados")
 valor_01 = int(input("Digite o primeiro valor: "))
-#print("\n")
 valor_02 = int(input("Digite o segundo valor: "))
-#print("\n")
+
+# Somando os valores
 valor_total = valor_01 + valor_02
 print(f"A soma dos valores é: {valor_total}") 
