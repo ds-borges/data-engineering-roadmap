277b040139f4f6713778b7658ca4e6fb18466c68
ds-borges <diego97sousa@gmail.com>
2025-08-26 09:05:22 -0300

    criando o exercicio 04

---
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_04.py
@@ -0,0 +1,3 @@
+############################################### Exercício 04 ###############################################
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo. #
