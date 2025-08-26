ba393d7712637d158249a11dee27b54f3ae9371c
ds-borges <diego97sousa@gmail.com>
2025-08-26 09:03:45 -0300

    exercicio 3 final

--- Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_03.py
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_03.py
@@ -2,3 +2,11 @@
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado. #
 
+# Pegando os valores
+valor_01 = int(input("Digite o primeiro valor: "))
+valor_02 = int(input("Digite o segundo valor: "))
+
+# Multiplicando os valores
+valor_total = valor_01 * valor_02
+
+print(f"A multiplicação dos valores é: {valor_total}") 
