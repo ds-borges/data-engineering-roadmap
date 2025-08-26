acd4dc7b4679081d231fbd4f7bbc9c62d48e756c
ds-borges <diego97sousa@gmail.com>
2025-08-26 09:23:52 -0300

    exercicio 2 correção

--- Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_02.py
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_02.py
@@ -2,7 +2,7 @@
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5. #
 
-
+# Pegar os valores
 print("Vamos dividir o resto da divisão por 5")
 valor = int(input("Digite o valor: "))
 
