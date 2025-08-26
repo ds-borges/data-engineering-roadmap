2fcd56860d6487a1c5da1d317d28841491f674cf
ds-borges <diego97sousa@gmail.com>
2025-08-26 10:12:31 -0300

    criando o exercicio 10

---
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_10.py
@@ -0,0 +1,4 @@
+############################################### Exercício 10 ###############################################
+
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
