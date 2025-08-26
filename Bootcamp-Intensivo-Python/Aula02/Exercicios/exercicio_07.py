92a46cb6e39071168a208245808bd23fc19e330f
ds-borges <diego97sousa@gmail.com>
2025-08-26 09:33:24 -0300

    criando o exercicio 07

---
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_07.py
@@ -0,0 +1,4 @@
+######################################### Exercício 07 #########################################
+
+# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário. #
+
