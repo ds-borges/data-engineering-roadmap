0c2e129dc7a25fc350c262d0fb2707df2846c1fa
ds-borges <diego97sousa@gmail.com>
2025-08-26 09:42:36 -0300

    criando o exercicio 08

---
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_08.py
@@ -0,0 +1,3 @@
+############################################### Exercício 08 ###############################################
+
+# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário). #
