5f295f838528c72174a10ee6980d9adcb55bea49
ds-borges <diego97sousa@gmail.com>
2025-08-26 09:08:20 -0300

    exercicio 4 final

--- Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_04.py
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_04.py
@@ -1,3 +1,12 @@
 ############################################### Exercício 04 ###############################################
 
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo. #
+
+# Pegando os valores
+valor_01 = int(input("Digite o primeiro valor: "))
+valor_02 = int(input("Digite o segundo valor: "))
+
+# Dividindo o primeiro pelo segundo
+valor_total = valor_01 / valor_02
+
+print(f"A divisão do primeiro pelo segundo é: {valor_total}") 
