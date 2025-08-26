d109ace2ebbcae783d6c4ee9790307b6e131fd9c
ds-borges <diego97sousa@gmail.com>
2025-08-26 09:24:23 -0300

    exercicio 4 correção

--- Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_04.py
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_04.py
@@ -3,6 +3,7 @@
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo. #
 
 # Pegando os valores
+print(" Dividindo 2 valores")
 valor_01 = int(input("Digite o primeiro valor: "))
 valor_02 = int(input("Digite o segundo valor: "))
 
