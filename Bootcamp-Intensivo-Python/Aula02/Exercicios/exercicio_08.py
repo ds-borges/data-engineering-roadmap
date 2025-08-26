ed9499df9aa49ba5fb7365e9da6f7e7f837ca089
ds-borges <diego97sousa@gmail.com>
2025-08-26 09:49:41 -0300

    exercicio 8 final

--- Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_08.py
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_08.py
@@ -1,3 +1,14 @@
 ############################################### Exercício 08 ###############################################
 
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário). #
+
+# Pegando os valores
+print("Vamos Vamos calcular a potência de um número")
+valor_01 = int(input("Digite a base desejado: "))
+valor_02 = int(input("Digite o expoente desejado: "))
+
+# Realizando a potência do número A base sempre significa o valor que vão O número vai ser repetido
+#valor_total = valor_01 ** valor_02
+valor_total = pow(valor_01, valor_02)
+
+print(f"O número {valor_01} elevado a {valor_02} é: {valor_total}") 
