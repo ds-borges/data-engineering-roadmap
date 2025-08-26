309e4fb70fd295f2fb3b277a7f5ea544313efd89
ds-borges <diego97sousa@gmail.com>
2025-08-26 09:24:08 -0300

    exercicio 3 correção

--- Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_03.py
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_03.py
@@ -3,6 +3,7 @@
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado. #
 
 # Pegando os valores
+print(" Multiplicando 2 valores")
 valor_01 = int(input("Digite o primeiro valor: "))
 valor_02 = int(input("Digite o segundo valor: "))
 
