c8dd74c46e4b9d40dbeb9e3c23bb35198fa633cf
ds-borges <diego97sousa@gmail.com>
2025-08-26 10:16:21 -0300

    exercicio 10 final

--- Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_10.py
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_10.py
@@ -2,3 +2,13 @@
 
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
+import math
+
+# Pegando o valor do raio
+print("Vamos calcular a área de um círculo")
+raio = int(input("Digite o valor do raio: "))
+
+# Calculando a área do círculo e arredondando o resultado para ter 2 casas decimais
+area = round(math.pi * pow(raio, 2), 2)
+
+print(f" O circulo de raio {raio} tem a área de {area}") 
