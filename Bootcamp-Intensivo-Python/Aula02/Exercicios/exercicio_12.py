d979e6718c39a6ff033eb963b88d99776d639fbf
ds-borges <diego97sousa@gmail.com>
2025-08-26 10:28:24 -0300

    criando o exercicio 12

---
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_12.py
@@ -0,0 +1,4 @@
+################################################# Exercício 12 ################################################
+
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas. #
+
