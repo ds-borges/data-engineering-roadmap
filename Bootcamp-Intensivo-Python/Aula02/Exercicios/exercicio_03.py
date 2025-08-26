8afcc7eb0d55df850d8af395d8bef5d5e5e2cc1b
ds-borges <diego97sousa@gmail.com>
2025-08-26 09:02:03 -0300

    criando o exercicio 03

---
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_03.py
@@ -0,0 +1,4 @@
+############################################# Exercício 03 #############################################
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado. #
+
