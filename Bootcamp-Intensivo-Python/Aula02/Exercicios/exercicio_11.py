2a0cd9a9529102af17fcec431847ce3d3bed237d
ds-borges <diego97sousa@gmail.com>
2025-08-26 10:17:23 -0300

    criando o exercicio 11

---
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_11.py
@@ -0,0 +1,4 @@
+############################################### Exercício 11 ###############################################
+
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
