27f4c5c671a26867bc73402c4f9897fca7a1f4e5
ds-borges <diego97sousa@gmail.com>
2025-08-26 10:26:53 -0300

    exercicio 10 comentários corrigido

--- Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_10.py
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_10.py
@@ -1,6 +1,6 @@
-############################################### Exercício 10 ###############################################
+####################################### Exercício 10 #######################################
 
-# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada. #
 
 import math
 
