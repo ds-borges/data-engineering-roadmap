eee5301c90d9150f5d1a3e93fa49a4d7bcb42e3a
ds-borges <diego97sousa@gmail.com>
2025-08-27 12:00:21 -0300

    criando o exercicio 20

---
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_20.py
@@ -0,0 +1,3 @@
+########################################## Exercício 20 #########################################
+
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes. #
