8e56b31e28bdab1118f5a7fe6a4fdcb02578206b
ds-borges <diego97sousa@gmail.com>
2025-08-27 11:32:16 -0300

    criando o exercicio 17

---
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_17.py
@@ -0,0 +1,3 @@
+############################################### Exercício 17 ##############################################
+
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR. #
