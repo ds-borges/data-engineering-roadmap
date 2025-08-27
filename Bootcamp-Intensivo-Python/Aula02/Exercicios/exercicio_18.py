f392bd33aed6689252b574d6e787f4870073d176
ds-borges <diego97sousa@gmail.com>
2025-08-27 11:40:13 -0300

    exercicio 18 final

--- Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_18.py
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_18.py
@@ -2,3 +2,11 @@
 
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor. #
 
+# Pegando os valores
+print("Invertendo as expressões boleanas")
+expression = (input("Digite a expressão boleana (true ou false): "))
+
+# Garantir que as entradas são "true" ou "false" e já inverter elas
+expression = not (expression.lower() == "true")
+
+print(f"O resultado da inversão da expressão boleana é: {expression}") 
