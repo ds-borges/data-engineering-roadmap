a1791ff77e280df4eb3f13fe96df977685f6e94b
ds-borges <diego97sousa@gmail.com>
2025-08-27 12:20:23 -0300

    exercicio 21 final

--- Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_21.py
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_21.py
@@ -2,3 +2,14 @@
 
 # 21: Conversor de Temperatura #
 
+print("Conversor de temperatura de Celsius para Fahrenheit")
+while True:
+    try:
+        celsius = float(input("Digite a temperatura em Celsius: "))
+        break
+    except ValueError:
+        print("Digite apenas um valor para converter !!\n")
+
+# Convertendo para Fahrenheit e arredondando para ter apenas 2 casas decimais
+Fahrenheit = round((celsius * 1.8) +32, 2)
+print(f"A temperatura {celsius} em Celsius é igual {Fahrenheit} em Fahrenheit") 
