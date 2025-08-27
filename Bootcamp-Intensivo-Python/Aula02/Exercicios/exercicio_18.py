b4cb134d6518e2b78e6a551f0f9440a1b11e33db
ds-borges <diego97sousa@gmail.com>
2025-08-27 11:35:58 -0300

    criando o exercicio 18

---
+++ Bootcamp-Intensivo-Python/Aula02/Exercicios/exercicio_18.py
@@ -0,0 +1,4 @@
+################################################### Exercício 18 ###################################################
+
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor. #
+
