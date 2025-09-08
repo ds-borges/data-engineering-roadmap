43ef0f4647d70f86edd73bc7428d4d0097dd8d30
ds-borges <diego97sousa@gmail.com>
2025-09-07 21:30:17 -0300

    adding docstring

--- Bootcamp-Intensivo-Python/aula04/exercicios/exercise01.py
+++ Bootcamp-Intensivo-Python/aula04/exercicios/exercise01.py
@@ -1,6 +1,22 @@
-# ### Exercício 1: Lista de Números ao Quadrado
-# **Objetivo:** Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+"""
+Exercício 1: Lista de Números ao Quadrado
 
-numbers = list(range(1,11))
+Objetivo:
+    Elevar ao quadrado cada número de 1 a 10 e imprimir o resultado.
+
+Instruções:
+    - O programa cria uma lista de números inteiros de 1 a 10.
+    - Ele percorre a lista e, para cada número, calcula o seu quadrado.
+    
+Saída Esperada:
+    O programa imprimirá uma lista de 10 linhas,
+    mostrando o número original e o seu valor ao quadrado.
+"""
+
+# Cria uma lista de números inteiros de 1 a 10 usando a função `range()`.
+numbers: list = list(range(1, 11))
+
+# Percorre a lista `numbers` usando um loop `for`.
 for number in numbers:
+    # Calcula o quadrado do número atual e imprime a mensagem formatada.
     print(f"O número {number} ao quadrado é {number**2}")
