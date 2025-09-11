7b522e523a4d89dd367cae6674ae49305834aeb7
ds-borges <diego97sousa@gmail.com>
2025-09-11 12:11:52 -0300

    adding pyproject

---
+++ Bootcamp-Intensivo-Python/aula06/main.py
@@ -0,0 +1,4 @@
+print("exemplo 01\n")
+# teste ttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
+print("Seja bem vindo(a): " + input("Digite seu nome: ") + " !")
+print(3 + 4)
