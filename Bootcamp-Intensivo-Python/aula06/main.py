4fa85afd0d885aee2a921b66aa086643fdea2300
ds-borges <diego97sousa@gmail.com>
2025-09-11 12:20:34 -0300

    included pre-commit

---
+++ Bootcamp-Intensivo-Python/aula06/main.py
@@ -0,0 +1,5 @@
+# Adcionando o pre-commit neste repositório
+#########################################################################################
+# I used the pyproject file and the pre-commit-config
+# Flake8 file was included but doesn"t work
+# So I decided to add args to the pre-commit-config
