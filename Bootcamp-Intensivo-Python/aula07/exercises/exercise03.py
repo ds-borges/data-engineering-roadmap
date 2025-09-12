3319b4ab6e1a728709a1b4363e5628af7d3a2e1b
ds-borges <diego97sousa@gmail.com>
2025-09-12 15:53:29 -0300

    fix: print about the numbers input

--- Bootcamp-Intensivo-Python/aula07/exercises/exercise03.py
+++ Bootcamp-Intensivo-Python/aula07/exercises/exercise03.py
@@ -11,10 +11,9 @@ def contar_valores_unicos(lista: List[int]) -> int:
 
 
 print("Bem vindo a exclusor de duplicatas!!")
-print("Avisos o analisador aceita todo tipo de números")
+print("Avisos o analisador aceita apenas números inteiros")
 
 recebendo_numeros: List[float] = receiving_value()
 numeros_int: List[int] = [int(num) for num in recebendo_numeros]
-# unique_numbers = contar_valores_unicos(recebendo_numeros)
 
 print(f"\nA quantidade de valores únicos são: {contar_valores_unicos(numeros_int)}")
