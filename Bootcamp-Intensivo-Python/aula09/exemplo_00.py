195f47c7a210b06559414791a2d3fee56ea39460
ds-borges <diego97sousa@gmail.com>
2025-09-22 14:46:10 -0300

    example of using the time_meassure_decorator in a function

---
+++ Bootcamp-Intensivo-Python/aula09/exemplo_00.py
@@ -0,0 +1,22 @@
+import time
+
+from timer import time_measure_decorator
+
+# Configuração do logger para stderr
+# logger.add(
+#                sink=stderr,
+#                format="{time} <r>{level}</r> <g>{message}</g> {file}",
+#                level="INFO"
+#          )
+
+
+@time_measure_decorator
+def soma(x, y):
+    soma = x + y
+    time.sleep(2)
+    print(f"Voce digitou valores em formato corretor, parabens {soma}")
+    return soma
+
+
+soma(2, 7)
+soma(2, 5)
