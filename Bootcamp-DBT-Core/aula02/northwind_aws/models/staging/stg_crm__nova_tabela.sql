1065bbded4a28b2e88fee013c174a5804ddeeae9
ds-borges <diego97sousa@gmail.com>
2026-02-16 12:44:24 -0300

    fix: name nova_tabela column

---
+++ Bootcamp-DBT-Core/aula02/northwind_aws/models/staging/stg_crm__nova_tabela.sql
@@ -0,0 +1,12 @@
+with renamed as (
+    select
+        category_id as id,
+        category_name as name,
+        description,
+        picture,
+        updated_at
+    from
+    {{ref('raw_crm__nova_tabela')}}
+)
+
+select * from renamed
